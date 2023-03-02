7af91bebc201e79d71b7c3f7fda92e364d9a268c
Wanjiru Murira <wanjirumurira@gmail.com>
2023-03-02 18:01:39 +0300

    status code

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
 from fastapi.params import Body
 from typing import Optional
+from random import randrange
 
-
+my_posts =[{"title": "title of post 1", "content":"content of post 1", "id" :1}]
 
 app = FastAPI()
-
+#used to create the schema of our APIS
 class Post(BaseModel):
     title: str
     content : str
     published : bool = True
     rating: Optional[int] = None
 
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 
 @app.get("/")
 def get_posts():
-    return {"data" : "This is your posts"}
+    return {"data" : my_posts}
 
-@app.post("/createposts")
+@app.post("/")
+def get_posts():
+    return {"data" : my_posts}
+
+@app.post("/posts")
 def create_posts(post: Post):
-    print(post)
-    print(post.dict())
-    return{"data": "new post"}
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,1000000000)
+    my_posts.append(post_dict)
+    return{"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(int(id))
+    if not post:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return {"post_detail": post}
+
